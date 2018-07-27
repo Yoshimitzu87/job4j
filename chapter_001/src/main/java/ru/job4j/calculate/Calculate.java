80ed5a6e1c6f12d671762795e97fd00b712dc541
Yoshmitzu87 <spitfires111@gmail.com>
2018-07-27 19:11:49 +0300

    1.3. JavaDoc. Code convention

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,3 +1,10 @@
+/**
+* Package for calculate task.
+*
+* @author Anton Frolov (mailto:spitfires111@gmail.com)
+* @version $Id$
+* @since 0.1
+*/
 package ru.job4j.calculate;
 
 public class Calculate {
