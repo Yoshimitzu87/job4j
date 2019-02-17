1cacecbeafd5f3aebb115015f81aaab0963b61fd
Yoshmitzu87 <spitfires111@gmail.com>
2019-02-17 21:08:01 +0300

    максимум из трех чисел.

--- chapter_001/src/main/java/ru/job4j/max/Max.java
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -1,11 +1,13 @@
 package ru.job4j.max;
-
+/**
+ * @author Anton Frolov (mailto:spitfires111@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 
 public class Max {
 
-    public boolean max(int first, int second, int third) {
-        return first > second || first > third || second > third || third > first;
+    public int max(int first, int second, int third) {
+        return Math.max(Math.max(first,second),third);
     }
-
 }
-
