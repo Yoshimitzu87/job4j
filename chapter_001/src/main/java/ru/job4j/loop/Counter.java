a933f8d5c7142f5d5cbf25889b27888d90a5d581
Yoshmitzu87 <spitfires111@gmail.com>
2019-02-18 19:08:52 +0300

    5.1 подсчет суммы чётных чисел.

---
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -0,0 +1,18 @@
+package ru.job4j.loop;
+
+/**
+ *
+ */
+public class Counter {
+    public int add(int start, int finish) {
+        int i = 0;
+        for (int index = start; index <= finish; index++) {
+            if (index % 2 == 0) {
+                i += index;
+                continue;
+            }
+        }
+        return i;
+    }
+}
+
---
+++ chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class CounterTest {
+    @Test
+    public void whenSumEvenNumbersFromOneToTenThenThirty() {
+        Counter add = new Counter();
+        int start = 1;
+        int finish = 10;
+        int i;
+        assertThat(i ,is(30));
+    }
+}
