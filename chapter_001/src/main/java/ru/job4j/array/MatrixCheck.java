4d09b0292792e88e581ca9db4b6b586940cae59e
Yoshmitzu87 <spitfires111@gmail.com>
2019-03-27 11:27:56 +0300

    6.7. Квадратный массив заполнен true или false по диагоналям.

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -1,6 +1,8 @@
 package ru.job4j.array;
+
 /**
  * MatrixCheck
+ *
  * @author Anton Frolov
  * @version $Id$
  * @since 0.1 (26.03.2019)
@@ -9,14 +11,15 @@ package ru.job4j.array;
 public class MatrixCheck {
     public boolean mono(boolean[][] data) {
         boolean result = true;
-        for (int i = 0; i < data.length; i++)
-            for (int j = 0; j < data.length; j++) {
-                if (data[i][j] != data[i+1][j+1] || data[i][j+2] != data[i+1][j+1])
-                {result = false;
+        for (int i = 0; i < data.length; i++) {
+            if (data[i][i] != data[i + 1][i + 1] ||
+                data[i][i + 2] != data[i + 1][i + 1] ||
+                data[i + 2][i] != data[i + 1][i + 1]) {
+                result = false;
 
-                    break;}
-
+                break;
             }
+        }
         return result;
     }
 }
--- chapter_001/src/test/java/ru/job4j/array/MatrixCheckTest.java
+++ chapter_001/src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -8,6 +8,7 @@ package ru.job4j.array;
  */
 
 import org.junit.Test;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -15,7 +16,7 @@ public class MatrixCheckTest {
     @Test
     public void whenDataMonoByTrueThenTrue() {
         MatrixCheck check = new MatrixCheck();
-        boolean[][] input = new boolean[][] {
+        boolean[][] input = new boolean[][]{
                 {true, true, true},
                 {false, true, true},
                 {true, false, true}
@@ -27,7 +28,7 @@ public class MatrixCheckTest {
     @Test
     public void whenDataNotMonoByTrueThenFalse() {
         MatrixCheck check = new MatrixCheck();
-        boolean[][] input = new boolean[][] {
+        boolean[][] input = new boolean[][]{
                 {true, true, false},
                 {false, false, true},
                 {true, false, true}
