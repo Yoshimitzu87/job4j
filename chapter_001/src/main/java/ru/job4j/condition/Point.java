b9bbd7b08394dca6be7763ecb975217c7093f1a6
Yoshmitzu87 <spitfires111@gmail.com>
2019-04-08 12:52:42 +0300

    1. Рефакторинг - Расстояние между точками. [#122587]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -1,36 +1,44 @@
 package ru.job4j.condition;
 
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
 /**
  * Point.
+ *
  * @author Anton Frolov (spitfires111@gmail.com)
  * @version $Id$
  * @since 0.1
  */
+
 public class Point {
+
+    /**
+     * Это поле объекта. Оно доступно только конкретному объекту.
+     */
     private int x;
+
+    /**
+     * И это поле объекта. Оно доступно только конкретному объекту.
+     */
     private int y;
 
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
+    /**
+     * Конструтор, который принимает начальное состояние объекта "точка"
+     *
+     * @param first  координата x
+     * @param second координата y
+     */
+    public Point(int first, int second) {
+        this.x = first;
+        this.y = second;
     }
 
-    public double distanceTo(Point that) {
-        return Math.sqrt(
-                Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2)
-        );
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
 
-    public static void main(String[] args) {
-        Point a = new Point(0, 1);
-        Point b = new Point(2, 5);
-        // сделаем вызов метода
-        System.out.println("x1 = " + a.x);
-        System.out.println("y1 = " + a.y);
-        System.out.println("x2 = " + b.x);
-        System.out.println("y2 = " + b.y);
-
-        double result = a.distanceTo(b);
-        System.out.println("Расстояние между точками А и В : " + result);
+    public void info() {
+        System.out.println(String.format("Point[%s, %s]", this.x, this.y));
     }
 }
