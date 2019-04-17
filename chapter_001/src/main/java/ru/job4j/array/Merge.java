c90dbd68e1cfcb3e4e5bafeeeb5147ad5446073e
Yoshmitzu87 <spitfires111@gmail.com>
2019-04-17 21:13:10 +0300

    Мерж

--- chapter_001/src/main/java/ru/job4j/array/Merge.java
+++ chapter_001/src/main/java/ru/job4j/array/Merge.java
@@ -1,9 +1,6 @@
 package ru.job4j.array;
 
 import java.util.Arrays;
-import sun.reflect.generics.tree.ArrayTypeSignature;
-
-import java.util.concurrent.atomic.AtomicStampedReference;
 
 /**
  * сортировка
@@ -14,28 +11,21 @@ import java.util.concurrent.atomic.AtomicStampedReference;
  */
 
 
-
-
-
 public class Merge {
     public static void main(String[] args) {
-        int[] a = {1, 3};
-        int[] b = {2, 4};
-        int length = a.length + b.length;
-        int[] result = new int[length];
+        int[] a = {1, 3, 5, 7, 9};
+        int[] b = {2, 4, 6, 8, 10};
+        int[] result = new int[a.length + b.length];
         System.arraycopy(a, 0, result, 0, a.length);
         System.arraycopy(b, 0, result, a.length, b.length);
-        for (int i = 0; i < result.length - 1; i++) {
+        for (int barrier = result.length - 1; barrier >= 0; barrier--) {
+            for (int i = 0; i < result.length - 1; i++) {
                 if (result[i] > result[i + 1]) {
                     int tmp = result[i];
                     result[i] = result[i + 1];
                     result[i + 1] = tmp;
-
                 }
-
             }
-            System.out.println(Arrays.toString(result));
-
-        }
-
+            }System.out.println(Arrays.toString(result));
     }
+}
