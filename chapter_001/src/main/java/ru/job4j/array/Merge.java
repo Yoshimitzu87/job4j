52f9fde91040533863237406f9d2cf2c78557d59
Yoshmitzu87 <spitfires111@gmail.com>
2019-04-17 19:52:35 +0300

    Мерж

--- chapter_001/src/main/java/ru/job4j/array/Merge.java
+++ chapter_001/src/main/java/ru/job4j/array/Merge.java
@@ -1,5 +1,10 @@
 package ru.job4j.array;
 
+import java.util.Arrays;
+import sun.reflect.generics.tree.ArrayTypeSignature;
+
+import java.util.concurrent.atomic.AtomicStampedReference;
+
 /**
  * сортировка
  *
@@ -14,22 +19,23 @@ package ru.job4j.array;
 
 public class Merge {
     public static void main(String[] args) {
-
         int[] a = {1, 3};
         int[] b = {2, 4};
         int length = a.length + b.length;
         int[] result = new int[length];
         System.arraycopy(a, 0, result, 0, a.length);
         System.arraycopy(b, 0, result, a.length, b.length);
-        for (int i = 0; i < result.length - 1; i--) {
-            for (int j = 0; j < i; j++) {
+        for (int i = 0; i < result.length - 1; i++) {
+                if (result[i] > result[i + 1]) {
+                    int tmp = result[i];
+                    result[i] = result[i + 1];
+                    result[i + 1] = tmp;
 
-                if (result[j] > result[j + 1]) {
-                    int tmp = result[j];
-                    result[j] = result[j + 1];
-                    result[j + 1] = tmp;
                 }
+
             }
+            System.out.println(Arrays.toString(result));
+
         }
+
     }
-}
