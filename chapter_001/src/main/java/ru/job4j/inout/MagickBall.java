8f24ea4df3c320fe72a9bf69dd891dc275cae626
Yoshmitzu87 <spitfires111@gmail.com>
2019-09-12 03:18:35 +0300

    1. Магический шар. [#179514]

---
+++ chapter_001/src/main/java/ru/job4j/inout/MagickBall.java
@@ -0,0 +1,19 @@
+package ru.job4j.inout;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class MagickBall {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Я великий Оракул. Что ты хочешь узнать? Задай вопрос на который можно ответить либо да, либо нет. ");
+        String a = input.nextLine();
+        int answer = new Random().nextInt(3);
+         if (answer == 0)
+             System.out.println("Да");
+         else if (answer == 1)
+             System.out.println("Нет");
+         else if (answer == 2)
+             System.out.println("Возможно");
+    }
+}
