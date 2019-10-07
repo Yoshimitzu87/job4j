bbdb3fb2c3068b59a6035ca91d459b0f25a2ef3b
Yoshmitzu87 <spitfires111@gmail.com>
2019-10-07 18:10:00 +0300

    2. Игра 11. Циклы и Scanner [#179518][#179786]

--- chapter_001/src/main/java/ru/job4j/inout/MatchesGame.java
+++ chapter_001/src/main/java/ru/job4j/inout/MatchesGame.java
@@ -1,30 +1,28 @@
 package ru.job4j.inout;
-
 import java.util.Scanner;
 
 public class MatchesGame {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
         int pool = 11;
-        System.out.println("Всего есть 11 спичек");
-        System.out.println("вы берёте от 1-й до 3-х спичек по очереди");
-        System.out.println("игра заканчивается когда игрок забрал последние спички");
+        System.out.println("Игрок 1, введите своё имя:");
+        String name1 = scan.nextLine();
+        System.out.println("Игрок 2, введите своё имя:");
+        String name2 = scan.nextLine();
+        boolean isPlayer = false;
         System.out.println("Вводите количество спичек которые вы хотите взять, по очереди");
-        System.out.print(  "игрок берет спичек: ");
         while (pool > 0) {
+            System.out.println((isPlayer ? name2 : name1) + ", сколько берёте спичек? ");
             int select = input.nextInt();
-            input.nextLine(); // дочитываем строку до конца.
-                if (select == 1) {
+            if (select <= 3) {
                     pool = pool - select;
                     System.out.println("осталось спичек: " + pool);
-                } else if (select == 2) {
-                    pool = pool - select;
-                    System.out.println("осталось спичек: " + pool);
-                } else if (select == 3) {
-                    pool = pool - select;
-                    System.out.println("осталось спичек: " + pool);
+                    isPlayer = !isPlayer;
+                } else if (select > 3) {
+                    System.out.println("Одну,Две или Три спички");
                 } if (pool == 0) {
-                    System.out.println("Победил последний игрок!!!");
+                    System.out.println("Победитель " + (isPlayer ? name2 : name1) + "!");
                 }
 
             }
