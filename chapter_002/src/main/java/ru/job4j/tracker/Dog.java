1f67ceb0f75e9797e59fefb73b070cfdd7de929d
Yoshmitzu87 <spitfires111@gmail.com>
2019-11-06 23:10:05 +0300

    1.1. Создание объекта [#165452]

---
+++ chapter_002/src/main/java/ru/job4j/tracker/Dog.java
@@ -0,0 +1,12 @@
+package ru.job4j.tracker;
+
+public class Dog {
+    public static void main(String[] args) {
+        Dog polkan = new Dog();
+
+        Dog zhucka = new Dog();
+
+        Dog sharick = new Dog();
+    }
+}
+
