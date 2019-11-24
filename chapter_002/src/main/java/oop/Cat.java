9bf672ece3d915542a20a62494cba29dcbcd5542
Yoshmitzu87 <spitfires111@gmail.com>
2019-11-24 03:15:06 +0300

    Маx для 3х чисел

--- chapter_002/src/main/java/oop/Cat.java
+++ chapter_002/src/main/java/oop/Cat.java
@@ -3,14 +3,32 @@ package oop;
 
 public class Cat {
 
-    public String sound() {
-        String voice = "may-may";
-        return voice;
-    }
+        private String food;
+        private String name;
 
-    public static void main(String[] args) {
-        Cat peppy = new Cat();
-        String say = peppy.sound();
-        System.out.println("Peppy says " + say);
+        public void show() {
+            System.out.println(this.name + " Скушал " + this.food);
+        }
+
+        public void giveNick(String nick){
+            this.name = nick;
+
+        }
+
+        public void eat(String meat) {
+            this.food = meat;
+        }
+
+        public static void main(String[] args) {
+
+            Cat gav = new Cat();
+            gav.eat("kotleta");
+            gav.giveNick("GAV");
+            gav.show();
+
+            Cat black = new Cat();
+            black.giveNick("BLACK");
+            black.eat("fish");
+            black.show();
+        }
     }
-}
