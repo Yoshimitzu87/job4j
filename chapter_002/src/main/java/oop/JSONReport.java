827397d4b880c44fb7b571011eda88d16b8d698e
Yoshmitzu87 <spitfires111@gmail.com>
2019-12-21 02:32:28 +0300

    5. Аннотация @Override [#174891]

--- chapter_002/src/main/java/oop/JSONReport.java
+++ chapter_002/src/main/java/oop/JSONReport.java
@@ -2,6 +2,7 @@ package oop;
 
 public class JSONReport extends TextReport {
 
+    @Override
     public String generate(String name, String body) {
         return "name :" + name +
                 "body:" + body;
--- chapter_002/src/main/java/oop/TextReport.java
+++ chapter_002/src/main/java/oop/TextReport.java
@@ -1,6 +1,8 @@
 package oop;
 
 public class TextReport {
+
+    @Override
     public String generate(String name, String body) {
         return name + System.lineSeparator() + body;
     }
