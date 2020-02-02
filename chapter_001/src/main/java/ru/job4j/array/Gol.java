5f5b7ad57b8d7d44df360634b026dd92fd810999
Yoshmitzu87 <spitfires111@gmail.com>
2020-02-03 02:31:49 +0300

    03.02

--- chapter_001/src/main/java/ru/job4j/array/Gol.java
+++ chapter_001/src/main/java/ru/job4j/array/Gol.java
@@ -1,32 +1,15 @@
 package ru.job4j.array;
-import java.util.Scanner;
+
+import java.util.Random;
 
 public class Gol {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Игорёк 1, введите своё имя:");
-        String name1 = scan.nextLine();
-        System.out.println("Игорёк 2, введите своё имя:");
-        String name2 = scan.nextLine();
-        boolean isCurrentPlayer = false;
-        boolean playGame = true;
-        int matches = 11;
-        while (playGame) {
-            System.out.println((isCurrentPlayer ? name2 : name1) + ", сколько берёте спичек? (Можно от 1 до 3)");
-            int select = scan.nextInt();
-            scan.nextLine();
-            if (select > 3 || select <= 0) {
-                System.out.println("Можно от 1 до 3 !");
-            } else {
-                matches = matches - select;
-                if (matches <= 0) {
-                    playGame = false;
-                } else {
-                    System.out.println("Осталось " + matches + " спичек");
-                    isCurrentPlayer = !isCurrentPlayer;
-                }
-            }
-        }
-        System.out.println("Победитель " + (isCurrentPlayer ? name2 : name1) + "!");
+
+
+       double i =  (Math.random() * 2);
+
+
+        System.out.println(i);
     }
+
 }
