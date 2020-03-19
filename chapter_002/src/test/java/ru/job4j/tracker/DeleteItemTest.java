b9d653b3e625ccf414ee81b293125e001d0ddeba
Yoshmitzu87 <spitfires111@gmail.com>
2020-03-19 22:49:29 +0300

    5.1. Тестирование. Подготовка данных.

--- chapter_002/src/test/java/ru/job4j/tracker/DeleteItemTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/DeleteItemTest.java
@@ -2,22 +2,30 @@ package ru.job4j.tracker;
 
 
 import org.junit.Test;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 
-
 public class DeleteItemTest {
+    @Test
+    public void whenReplaceItem() {
+        Tracker tracker = new Tracker();
+        Item item = new Item("new item");
+        tracker.add(item);
+        String[] answers = { item.getId(), "replaced item"};
+        StartUI.replaceItem(new StubInput(answers), tracker);
+        Item replaced = tracker.findById(item.getId());
+        assertThat(replaced.getName(), is("replaced item"));
+    }
     @Test
     public void whenDeleteItem() {
         Tracker tracker = new Tracker();
         Item item = new Item("new item");
         tracker.add(item);
-        String[] answers = {
-                item.getId(),
-        };
+        String[] answers = {item.getId()};
         StartUI.deleteItem(new StubInput(answers), tracker);
         Item deleted = tracker.findById(item.getId());
-        assertThat(deleted, is("null"));
+        assertNull(deleted);
     }
 }
