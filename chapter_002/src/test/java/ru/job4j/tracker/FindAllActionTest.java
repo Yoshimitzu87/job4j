b1e2d1a91666e1ff09f849031f35c5fa9b93db1e
Yoshmitzu87 <spitfires111@gmail.com>
2020-03-25 23:59:11 +0300

    9.2. Тесты вывода на консоль в StartUI [#33585]

---
+++ chapter_002/src/test/java/ru/job4j/tracker/FindAllActionTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.StringJoiner;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class FindAllActionTest {
+    @Test
+    public void whenFindByName() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        PrintStream def = System.out;
+        System.setOut(new PrintStream(out));
+        Tracker tracker = new Tracker();
+        Item item = new Item("test1");
+        tracker.add(item);
+        StubInput input = new StubInput(new String[]{"test1"});
+        FindAllAction act = new FindAllAction();
+        act.execute(input, tracker);
+        String expect = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add(item.getId() + " " + item.getName())
+                .toString();
+        assertThat(new String(out.toByteArray()), is(expect));
+        System.setOut(def);
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/tracker/FindByNameActionTest.java
@@ -0,0 +1,30 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.StringJoiner;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class FindByNameActionTest {
+    @Test
+    public void whenFindByName() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        PrintStream def = System.out;
+        System.setOut(new PrintStream(out));
+        Tracker tracker = new Tracker();
+        Item item = new Item("testname");
+        tracker.add(item);
+        StubInput input = new StubInput(new String[]{"testname"});
+        FindByNameAction act = new FindByNameAction();
+        act.execute(input, tracker);
+        String expect = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add(item.getId() + " " + item.getName())
+                .toString();
+        assertThat(new String(out.toByteArray()), is(expect));
+        System.setOut(def);
+    }
+}
