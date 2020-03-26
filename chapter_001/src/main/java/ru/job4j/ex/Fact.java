93a2a1c41be13cdbfc17e0926b612b401ef69b88
Yoshmitzu87 <spitfires111@gmail.com>
2020-03-26 20:06:53 +0300

    0.3. Тестирование исключений с junit [#219366]

--- chapter_001/src/main/java/ru/job4j/ex/Fact.java
+++ chapter_001/src/main/java/ru/job4j/ex/Fact.java
@@ -5,7 +5,7 @@ public class Fact {
 
     }
 
-    public int calc(int n) {
+    public static int calc(int n) {
         if (n < 0){
             throw new IllegalArgumentException("Cant count with negative number");}
         int rsl = 1;
