bad1890e71c76b374ef3450b8c7167cde2fa0349
Yoshmitzu87 <spitfires111@gmail.com>
2020-04-12 17:54:28 +0300

    1. Уникальные задачи. [#212653]

---
+++ chapter_001/src/main/java/ru/job4j/collecion/FullSearch.java
@@ -0,0 +1,15 @@
+package ru.job4j.collecion;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class FullSearch {
+    public static HashSet<String> extractNumber(List <Task> list) {
+        HashSet<String> numbers = new HashSet<>();
+        for(Task n: list) {
+            numbers.add(n.getNumber());
+
+        }
+        return numbers;
+    }
+}
