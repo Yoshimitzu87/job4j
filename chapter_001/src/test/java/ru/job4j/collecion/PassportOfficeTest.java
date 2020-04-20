f3882390ea1cba31c6c1217794fedb4f568fb43e
Yoshmitzu87 <spitfires111@gmail.com>
2020-04-20 10:36:57 +0300

    1.Паспорт и жители.

---
+++ chapter_001/src/test/java/ru/job4j/collecion/PassportOfficeTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.collecion;
+
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class PassportOfficeTest {
+
+    @Test
+    public void add() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        assertThat(office.get(citizen.getPassport()), is(citizen));
+    }
+    @Test
+    public void add2() {
+        Citizen citizen = new Citizen("2f44a", "Petr Arsentev");
+        Citizen citizen2 = new Citizen("2f44a", "Vasya Pupkin");
+        PassportOffice office = new PassportOffice();
+        office.add(citizen);
+        Boolean addSameCitizen = office.add(citizen2);
+        assertFalse(addSameCitizen);
+    }
+}
