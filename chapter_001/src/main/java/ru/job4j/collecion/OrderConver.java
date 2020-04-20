403eba57ab16f9db1f458432b4736febd2c9aca3
Yoshmitzu87 <spitfires111@gmail.com>
2020-04-20 10:38:11 +0300

    1.Паспорт и жители.

---
+++ chapter_001/src/main/java/ru/job4j/collecion/OrderConver.java
@@ -0,0 +1,14 @@
+package ru.job4j.collecion;
+import java.util.HashMap;
+import java.util.List;
+
+public class OrderConver {
+    public static HashMap<String, Order> process(List<Order> orders) {
+        HashMap<String, Order> map = new HashMap<>();
+
+        for (Order ol : orders){
+            map.put(ol.getNumber(), ol);
+        }
+        return map;
+    }
+}
